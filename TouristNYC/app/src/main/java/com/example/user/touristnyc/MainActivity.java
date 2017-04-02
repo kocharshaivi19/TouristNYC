19a9ea8ce773f386abaa17aba2b5dab6a5f25aec
Tarik <tz638@nyu.edu>
2017-04-02 01:56:09 -0400

    Skeleton for user profile. Needs a lot of reworking.

--- TouristNYC/app/src/main/java/com/example/user/touristnyc/MainActivity.java
+++ TouristNYC/app/src/main/java/com/example/user/touristnyc/MainActivity.java
@@ -2,12 +2,77 @@ package com.example.user.touristnyc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected static int[] layoutIDs = {R.id.infoLayout, R.id.interestLayout, R.id.groupLayout};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewUserDetails();
+        viewPOIs();
+        viewGroups();
+    }
+
+    protected void viewUserDetails()  {
+
+        Button button = (Button) findViewById(R.id.detailsButton);
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    clearLayouts();
+                    LinearLayout infoLayout = (LinearLayout) findViewById(R.id.infoLayout);
+                    infoLayout.setVisibility(View.VISIBLE);
+                }
+            });
+        }
+    }
+
+    protected void viewPOIs()  {
+
+        Button button = (Button) findViewById(R.id.interestButton);
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    clearLayouts();
+                    LinearLayout interestLayout = (LinearLayout) findViewById(R.id.interestLayout);
+                    interestLayout.setVisibility(View.VISIBLE);
+                }
+            });
+        }
+    }
+
+    protected void viewGroups()  {
+
+        Button button = (Button) findViewById(R.id.groupsButton);
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    clearLayouts();
+                    LinearLayout groupLayout = (LinearLayout) findViewById(R.id.groupLayout);
+                    groupLayout.setVisibility(View.VISIBLE);
+                }
+            });
+        }
+    }
+
+    protected void clearLayouts()   {
+
+        for (int i=0; i<layoutIDs.length; i++)      {
+
+            LinearLayout layout = (LinearLayout) findViewById(layoutIDs[i]);
+            layout.setVisibility(View.GONE);
+        }
     }
 }
