2ebc0985cc00f8f77621eb436d860cf1b32ff048
root <brian_chuk@yahoo.com>
2017-04-09 23:20:15 -0400

    UI additions

--- TouristNYC/app/src/main/java/com/tourist/root/touristnyc/MainActivity.java
+++ TouristNYC/app/src/main/java/com/tourist/root/touristnyc/MainActivity.java
@@ -24,19 +24,19 @@ public class MainActivity extends AppCompatActivity {
 
         //Tab 1
         TabHost.TabSpec spec = host.newTabSpec("Tab One");
-        spec.setContent(R.id.tab1);
+        spec.setContent(R.id.explore);
         spec.setIndicator("Tab One");
         host.addTab(spec);
 
         //Tab 2
         spec = host.newTabSpec("Tab Two");
-        spec.setContent(R.id.tab2);
+        spec.setContent(R.id.trips);
         spec.setIndicator("Tab Two");
         host.addTab(spec);
 
         //Tab 3
         spec = host.newTabSpec("Tab Three");
-        spec.setContent(R.id.tab3);
+        spec.setContent(R.id.profile);
         spec.setIndicator("Tab Three");
         host.addTab(spec);
     }
