5531b6c26a86a4e3802260cc1bf9d2163bdfdd9e
root <brian_chuk@yahoo.com>
2017-04-12 15:25:41 -0400

    login

--- web/app.py
+++ web/app.py
@@ -28,5 +28,29 @@ def profile():
     return render_template("trips.html", username=session['username'])
 
 
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         session['username'] = request.form['username']
+#         return redirect(url_for('index'))
+#     return '''
+#         <form method="post">
+#             <p><input type=text name=username>
+#             <p><input type=submit value=Login>
+#         </form>
+#     '''
+
+
+# @app.route('/logout')
+# def logout():
+#     # remove the username from the session if it's there
+#     session.pop('username', None)
+#     return redirect(url_for('index'))
+
+
+# TODO
+# figure out how to connect SQL to Flask
+# write out ER diagram thing
+
 if __name__ == "__main__":
     app.run()
