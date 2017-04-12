a61e9f85b216253d1231beef34ed36519f3f72f1
root <brian_chuk@yahoo.com>
2017-04-12 14:51:54 -0400

    three pages in flask

--- web/app.py
+++ web/app.py
@@ -1,10 +1,32 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, session, redirect, url_for
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello(name=None):
-    return render_template("hello.html", name=name)
+def index():
+	return redirect(url_for('explore'));
+
+
+@app.route("/explore")
+def explore():
+    if 'username' in session:
+    	return render_template("explore.html", username=session['username'])
+    return render_template("explore.html", username=None)
+
+
+@app.route("/trips")
+def trips():
+    if 'username' in session:
+    	return render_template("trips.html", username=None)
+    return render_template("trips.html", username=session['username'])
+
+
+@app.route("/profile")
+def profile():
+    if 'username' in session:
+    	return render_template("trips.html", username=None)
+    return render_template("trips.html", username=session['username'])
+
 
 if __name__ == "__main__":
     app.run()
