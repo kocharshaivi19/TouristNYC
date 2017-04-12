ffa617f804f5ce96d34b796f968b189ed7503d83
root <brian_chuk@yahoo.com>
2017-04-12 14:21:50 -0400

    pivot

---
+++ web/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello(name=None):
+    return render_template("hello.html", name=name)
+
+if __name__ == "__main__":
+    app.run()
